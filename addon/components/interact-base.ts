61b22d2a05d6a1478ae146f4c3073f734ee910ea
simon <ihmig@kaliber5.de>
2018-08-17 16:39:05 +0200

    Split into base and element

---
+++ addon/components/interact-base.ts
@@ -0,0 +1,153 @@
+// @ts-ignore: Ignore import of compiled template
+import template from 'ember-interact/templates/components/interact-base';
+import Component from '@ember/component';
+import { attribute, layout } from '@ember-decorators/component';
+import { computed } from '@ember-decorators/object';
+import interact, {
+  DraggableOptions,
+  ResizableOptions,
+  Interactable,
+  OnEventName,
+  Listener
+} from 'interactjs';
+import { assign } from '@ember/polyfills';
+import { bind } from '@ember/runloop';
+
+type ActionName =
+  'onDragStart' |
+  'onDragMove' |
+  'onDragInertiaStart' |
+  'onDragEnd' |
+  'onResizeStart' |
+  'onResizeMove' |
+  'onResizeInertiaStart' |
+  'onResizeEnd' |
+  'onGestureStart' |
+  'onGestureMove' |
+  'onGestureEnd' |
+  'onDropActivate' |
+  'onDropDeactivate' |
+  'onDragEnter' |
+  'onDragLeave' |
+  'onDropMove' |
+  'onDrop' |
+  'onDown' |
+  'onMove' |
+  'onUp' |
+  'onCancel' |
+  'onTap' |
+  'onDoubleTap' |
+  'onHold';
+
+const eventActionMap: [OnEventName, ActionName][] = [
+    ['dragstart', 'onDragStart'],
+    ['dragmove', 'onDragMove'],
+    ['draginertiastart', 'onDragInertiaStart'],
+    ['dragend', 'onDragEnd'],
+    ['resizestart', 'onResizeStart'],
+    ['resizemove', 'onResizeMove'],
+    ['resizeinertiastart', 'onResizeInertiaStart'],
+    ['resizeend', 'onResizeEnd'],
+    ['gesturestart', 'onGestureStart'],
+    ['gesturemove', 'onGestureMove'],
+    ['gestureend', 'onGestureEnd'],
+    // drop
+    ['dropactivate', 'onDropActivate'],
+    ['dropdeactivate', 'onDropDeactivate'],
+    ['dragenter', 'onDragEnter'],
+    ['dragleave', 'onDragLeave'],
+    ['dropmove', 'onDropMove'],
+    ['drop', 'onDrop'],
+    // pointer events
+    ['down', 'onDown'],
+    ['move', 'onMove'],
+    ['up', 'onUp'],
+    ['cancel', 'onCancel'],
+    ['tap', 'onTap'],
+    ['doubletap', 'onDoubleTap'],
+    ['hold', 'onHold']
+];
+
+@layout(template)
+export default class InteractBase extends Component {
+  draggable: DraggableOptions | boolean = this.draggable || false;
+  resizable: ResizableOptions | boolean = this.resizable || false;
+  interactable?: Interactable;
+
+  @attribute
+  style?: string;
+
+  @computed('draggable')
+  get _draggable(): ResizableOptions | boolean {
+    return this.get('draggable');
+  }
+
+  @computed('resizable')
+  get _resizable(): ResizableOptions | boolean {
+    let orig = this.get('resizable');
+    let defaults: ResizableOptions = {
+      edges: { left: true, right: true, bottom: true, top: true }
+    };
+
+    if (orig === false) {
+      return false;
+    }
+    if (orig === true) {
+      return defaults;
+    }
+    return assign({}, defaults, orig);
+  }
+
+  setupInteractable() {
+    let interactable: Interactable;
+
+    interactable = interact(this.element)
+      .draggable(this.get('_draggable'))
+      .resizable(this.get('_resizable'));
+
+    eventActionMap
+      .forEach(([eventName, actionName]) => {
+        let fn = this[actionName];
+        if (typeof fn === 'function') {
+          interactable.on(eventName, bind(this, fn))
+        }
+      });
+
+    this.interactable = interactable;
+  }
+
+  onDragStart?: Listener;
+  onDragMove?: Listener;
+  onDragInertiaStart?: Listener;
+  onDragEnd?: Listener;
+  onResizeStart?: Listener;
+  onResizeMove?: Listener;
+  onResizeInertiaStart?: Listener;
+  onResizeEnd?: Listener;
+  onGestureStart?: Listener;
+  onGestureMove?: Listener;
+  onGestureEnd?: Listener;
+  onDropActivate?: Listener;
+  onDropDeactivate?: Listener;
+  onDragEnter?: Listener;
+  onDragLeave?: Listener;
+  onDropMove?: Listener;
+  onDrop?: Listener;
+  onDown?: Listener;
+  onMove?: Listener;
+  onUp?: Listener;
+  onCancel?: Listener;
+  onTap?: Listener;
+  onDoubleTap?: Listener;
+  onHold?: Listener;
+
+  didInsertElement() {
+    this.setupInteractable();
+  }
+
+  willDestroyElement() {
+    if (this.interactable) {
+      this.interactable.unset();
+    }
+  }
+};
--- addon/components/interact-element.ts
+++ addon/components/interact-element.ts
@@ -1,19 +1,16 @@
 // @ts-ignore: Ignore import of compiled template
 import template from 'ember-interact/templates/components/interact-element';
 import Component from '@ember/component';
-import { attribute, classNames, layout } from '@ember-decorators/component';
-import { computed } from '@ember-decorators/object';
-import interact, { DraggableOptions, ResizableOptions, InteractEvent, Interactable } from 'interactjs';
-import { assign } from '@ember/polyfills';
+import { layout, tagName } from '@ember-decorators/component';
+import { action, computed } from '@ember-decorators/object';
+import { DraggableOptions, ResizableOptions, InteractEvent } from 'interactjs';
 import { htmlSafe } from "@ember/string";
-import { bind } from '@ember/runloop';
 
+@tagName('')
 @layout(template)
-@classNames('interact-element')
 export default class InteractElement extends Component {
   draggable: DraggableOptions | boolean = this.draggable || false;
   resizable: ResizableOptions | boolean = this.resizable || false;
-  interactable?: Interactable;
 
   // @todo make these strictly one-way
   x: number = this.x || 0;
@@ -21,61 +18,12 @@ export default class InteractElement extends Component {
   width: number = this.width || 100;
   height: number = this.height || 100;
 
-  // @computed('draggable')
-  // get _draggable(): DraggableOptions | boolean {
-  //   let orig = this.get('draggable');
-  //   let defaults: DraggableOptions = {
-  //   };
-  //
-  //   if (orig === false) {
-  //     return false;
-  //   }
-  //   if (orig === true) {
-  //     return defaults;
-  //   }
-  //   return assign({}, defaults, orig);
-  // }
-
-  @computed('resizable')
-  get _resizable(): ResizableOptions | boolean {
-    let orig = this.get('draggable');
-    let defaults: ResizableOptions = {
-      edges: { left: true, right: true, bottom: true, top: true }
-    };
-
-    if (orig === false) {
-      return false;
-    }
-    if (orig === true) {
-      return defaults;
-    }
-    return assign({}, defaults, orig);
-  }
-
-  @attribute
   @computed('x', 'y', 'width', 'height')
   get style() {
     return htmlSafe(`transform: translate(${this.x}px, ${this.y}px); width: ${this.width}px; height: ${this.height}px`);
   }
 
-  setupInteractable() {
-    this.interactable = interact(this.element)
-      .draggable(this.get('draggable'))
-      .resizable(this.get('_resizable'))
-      .on('dragmove', bind(this, this.onMove))
-      .on('resizemove', bind(this, this.onResize));
-  }
-
-  didInsertElement() {
-    this.setupInteractable();
-  }
-
-  willDestroyElement() {
-    if (this.interactable) {
-      this.interactable.unset();
-    }
-  }
-
+  @action
   onMove(e: InteractEvent) {
     this.setProperties({
         x: this.get('x') + e.dx,
@@ -83,6 +31,7 @@ export default class InteractElement extends Component {
     });
   }
 
+  @action
   onResize(event: InteractEvent) {
     this.setProperties({
       x: this.get('x') + event.deltaRect.left,
