2d10f7e372cf0b1e79fcd16f7b6578f542d6ab2e
simon <ihmig@kaliber5.de>
2018-08-17 19:27:45 +0200

    Basic tests

--- tests/integration/components/interact-element-test.js
+++ tests/integration/components/interact-element-test.js
@@ -1,26 +1,30 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, find, triggerEvent } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | interact-element', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
-    await render(hbs`{{interact-element}}`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
+  test('it renders as a block', async function(assert) {
     await render(hbs`
       {{#interact-element}}
         template block text
       {{/interact-element}}
     `);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.dom('.interact').hasText('template block text');
   });
+
+  test('it receives position and scale props', async function(assert) {
+    await render(hbs`{{interact-element x=10 y=20 width=50 height=200}}`);
+
+    let rect = find('.interact').getBoundingClientRect();
+
+    assert.equal(rect.left, 10);
+    assert.equal(rect.top, 20);
+    assert.equal(rect.width, 50);
+    assert.equal(rect.height, 200);
+  });
+
 });
