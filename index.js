9bc59788d0ab43a6cdfa39cb2d529ed22f71c75d
simon <ihmig@kaliber5.de>
2018-08-21 11:36:49 +0200

    Migrate styles to (importable) Sass

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-interact'
+  name: 'ember-interact',
+  included(app) {
+    this._super.included.apply(this, arguments);
+
+    let hasSass = !!app.registry.availablePlugins['ember-cli-sass'];
+
+    // Don't include the precompiled css file if the user has Sass
+    if (!hasSass) {
+      this.import('vendor/ember-interact.css');
+    }
+  }
 };
